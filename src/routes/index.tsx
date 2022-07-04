6a8219d0129ec3df8550fe5268ad8fd828ee24d5
22-YanKaiky <yankaikys@gmail.com>
2022-07-03 22:42:34 -0300

    feat(src): add material-ui

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,9 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<p>Teste Página Inicial</p>} />
+      <Route path="/" element={<Button color="error">Teste Página Inicial</Button>} />
       <Route path="*" element={<Navigate to='/' />} />
     </Routes>
   )
