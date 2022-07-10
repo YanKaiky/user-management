07485150db3749a63e5cf00acb78089ede97ed0e
22-YanKaiky <yankaikys@gmail.com>
2022-07-10 15:19:56 -0300

    feat(DrawerContext): responsivity menu

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,12 @@
 import { Button } from '@mui/material';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { useAppThemeContext } from '../shared/contexts';
+import { useDrawerContext } from '../shared/contexts';
 const AppRoutes = () => {
-  const { toggleTheme } = useAppThemeContext();
+  const { toggleDrawerOpen } = useDrawerContext();
   
   return (
     <Routes>
-      <Route path="/" element={<Button variant="contained" color="primary" onClick={toggleTheme}>Toggle</Button>} />
+      <Route path="/" element={<Button variant="contained" color="primary" onClick={toggleDrawerOpen}>Toggle</Button>} />
       <Route path="*" element={<Navigate to='/' />} />
     </Routes>
   );
--- src/shared/components/menu/Menu.tsx
+++ src/shared/components/menu/Menu.tsx
@@ -1,6 +1,7 @@
-import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useTheme } from '@mui/material';
+import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
+import { useDrawerContext } from '../../contexts';
 
 interface IMenuProps {
   children: React.ReactNode;
@@ -8,10 +9,13 @@ interface IMenuProps {
 
 export const Menu: React.FC<IMenuProps> = ({ children }) => {
   const theme = useTheme();
+  const smDown = useMediaQuery(theme.breakpoints.down('sm'));
 
+  const { isDrawerOpen, toggleDrawerOpen } = useDrawerContext();
+  
   return (
     <>
-      <Drawer variant='permanent'>
+      <Drawer open={isDrawerOpen} variant={smDown ? 'temporary' : 'permanent'} onClose={toggleDrawerOpen}>
         <Box width={theme.spacing(28)} height='100%' display='flex' flexDirection='column'>
           <Box width="100%" height={theme.spacing(20)} display='flex' alignItems='center' justifyContent='center'>
             <Avatar sx={{ height: theme.spacing(12), width: theme.spacing(12) }} src='https://www.github.com/YanKaiky.png' />
@@ -32,7 +36,7 @@ export const Menu: React.FC<IMenuProps> = ({ children }) => {
         </Box>
       </Drawer>
 
-      <Box height='100vh' marginLeft={theme.spacing(28)}>
+      <Box height='100vh' marginLeft={smDown ? 0 : theme.spacing(28)}>
         {children}
       </Box>
     </>
