ecc359e1a1bffb8f682709da63cd8ba03cadd829
22-YanKaiky <yankaikys@gmail.com>
2022-07-31 22:35:52 -0300

    feat(environments): define internal variables

--- src/shared/environments/index.ts
+++ src/shared/environments/index.ts
@@ -1,1 +1,8 @@
-export { };
+import base_url from '../services/api_url';
+
+export const Environment = {
+  LINE_LIMIT: 10,
+  SEARCH_INPUT: 'Search...',
+  EMPTY_LISTING: 'No record found.',
+  BASE_URL: base_url
+};
