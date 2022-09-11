c290a979ed76557f9462e7a6439fd02dc3de8928
22-YanKaiky <yankaikys@gmail.com>
2022-09-11 03:29:46 -0300

    feat(PeopleForm): style

--- src/shared/components/forms/PeopleForm.tsx
+++ src/shared/components/forms/PeopleForm.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { FC, useEffect, useState } from 'react';
-import { Autocomplete, Box, Button, TextField } from '@mui/material';
+import { Autocomplete, Box, TextField } from '@mui/material';
 import { CitiesService, ICityData } from '../../services/cities/cities.service';
 import { IUpsertPeopleData } from '../../services/people/people.service';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
@@ -28,59 +28,67 @@ export const PeopleForm: FC<IFormData> = ({ setInput, sendRequest, date, setDate
       setCities(data);
     })();
   }, []);
-  
+
   return (
-    <form
-      onChange={setInput}
-      onSubmit={(e) => {
-        e.preventDefault();
-        sendRequest();
-      }}
-    >
-      <TextField value={data.name} name="name" placeholder="Name" />
+    <Box sx={{ m: 1 }} >
+      <form
+        onChange={setInput}
+        onSubmit={(e) => {
+          e.preventDefault();
+          sendRequest();
+        }}
+      >
+        <Box display='flex'>
+          <TextField sx={{ marginRight: 0.5 }} value={data.name} name="name" label="Name" fullWidth />
 
-      <TextField value={data.last_name} name="last_name" placeholder="Last name" />
+          <TextField sx={{ marginLeft: 0.5 }} value={data.last_name} name="last_name" label="Last name" fullWidth />
+        </Box>
 
-      <TextField value={data.email} name="email" placeholder="Email" />
+        <Box display='flex' marginTop={2}>
+          <TextField sx={{ marginRight: 0.5 }} value={data.email} name="email" label="Email" fullWidth />
 
-      <TextField value={data.cpf} name="cpf" placeholder="CPF" />
+          <TextField sx={{ marginLeft: 0.5 }} value={data.cpf} name="cpf" label="CPF" fullWidth />
+        </Box>
 
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DatePicker
-          label="Birth date"
-          value={date}
-          onChange={(newValue) => setDate(moment(newValue.$d).format('YYYY-MM-DD'))}
-          renderInput={(params) => <TextField {...params} value={data.birth_date} name="birth_date" />}
-        />
-      </LocalizationProvider>
-
-      <Autocomplete
-        options={cities}
-        getOptionLabel={(option: ICityData) => option.name}
-        onChange={(_, newValue) => setCityGuid(newValue?.guid)}
-        value={cityGuid}
-        renderOption={(props, option) => (
-          <Box
-            {...props}
-            key={option.guid}
-            component="li"
-            sx={{ '& > img': { mr: 2, flexShrink: 0 } }}
-          >
-            {`${option.name} (${option.uf})`}
-          </Box>
-        )}
-        renderInput={(params) =>
-          <TextField
-            {...params}
-            label="City"
-            name='city_guid'
-            onChange={setInput}
-            value={data.city_guid}
+        <Box display='flex' marginTop={2}>
+          <Autocomplete
+            options={cities}
+            getOptionLabel={(option: ICityData) => option.name}
+            onChange={(_, newValue) => setCityGuid(newValue?.guid)}
+            value={cityGuid}
+            sx={{ marginRight: 0.5 }}
+            fullWidth
+            renderOption={(props, option) => (
+              <Box
+                {...props}
+                key={option.guid}
+                component="li"
+                sx={{ '& > img': { mr: 2, flexShrink: 0 } }}
+              >
+                {`${option.name} (${option.uf})`}
+              </Box>
+            )}
+            renderInput={(params) =>
+              <TextField
+                {...params}
+                label="City"
+                name='city_guid'
+                onChange={setInput}
+                value={data.city_guid}
+              />
+            }
           />
-        }
-      />
 
-      <Button type='submit'>Submit</Button>
-    </form>
+          <LocalizationProvider sx={{ marginLeft: 0.5 }} dateAdapter={AdapterDayjs}>
+            <DatePicker
+              label="Birth date"
+              value={date}
+              onChange={(newValue) => setDate(moment(newValue.$d).format('YYYY-MM-DD'))}
+              renderInput={(params) => <TextField {...params} value={data.birth_date} name="birth_date" />}
+            />
+          </LocalizationProvider>
+        </Box>
+      </form>
+    </Box>
   );
 };
